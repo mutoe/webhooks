ca6f33fd5bc7fc7c627db681eb7d72d45a9e0a99
mutoe <mutoe@foxmail.com>
2019-11-03 22:30:48 +0800

    chore: add debug and remove useless code

--- bin/index.js
+++ bin/index.js
@@ -25,43 +25,41 @@ const config = {
 }
 
 async function initial () {
-  const configPath = path.join(os.homedir(), '/.config/webhooks.config.json')
-  if (!fs.existsSync(configPath)) {
-    config.port = await prompt({
-      type: 'number',
-      name: 'port',
-      message: `Enter the ${chalk.underline('port')} you deployed the webhooks use:`,
-      default: 80,
-      validate: (port) => port >= 20 && port <= 65535 ? true : 'port should between 20 and 65535',
-    }).then(answer => answer.port)
+  config.port = await prompt({
+    type: 'number',
+    name: 'port',
+    message: `Enter the ${chalk.underline('port')} you deployed the webhooks use:`,
+    default: 80,
+    validate: (port) => port >= 20 && port <= 65535 ? true : 'port should between 20 and 65535',
+  }).then(answer => answer.port)
 
-    config.secret = await prompt({
-      type: 'input',
-      name: 'secret',
-      message: 'Enter the secret you will use:',
-    }).then(answer => answer.secret)
+  config.secret = await prompt({
+    type: 'input',
+    name: 'secret',
+    message: 'Enter the secret you will use:',
+  }).then(answer => answer.secret)
 
-    const content = fs.readFileSync(path.join(__dirname, '../listener.js.template')).toString()
-    const result = handlebars.compile(content)(config)
-    fs.writeFileSync(path.join(process.cwd(), './listener.js'), result)
-    fs.writeFileSync(path.join(process.cwd(), './pull.sh'), config.bash)
+  const content = fs.readFileSync(path.join(__dirname, '../listener.js.template')).toString()
+  const result = handlebars.compile(content)(config)
+  fs.writeFileSync(path.join(process.cwd(), './listener.js'), result)
+  fs.writeFileSync(path.join(process.cwd(), './pull.sh'), config.bash)
 
-    console.log(`${chalk.green.bold('listener.js')} and ${chalk.green.bold('pull.sh')} is created.`)
-    console.log(`You can edit ${chalk.green.bold('pull.sh')} now.\n`)
-    console.log(`When your modifications are complete, you can launch the hook by executing the following command:\n`)
-    console.log(`${chalk.cyan('node listener.js')}\n`)
-    console.log(`You can also use other tools to deploy the service, for example ${chalk.underline('pm2')}, ${chalk.underline('nodemon')} or ${chalk.underline('forever')}.\n`)
-    console.log(`Thank you for using it and start coding happily! 😊`)
-  }
+  console.log(`${chalk.green.bold('listener.js')} and ${chalk.green.bold('pull.sh')} is created.`)
+  console.log(`You can edit ${chalk.green.bold('pull.sh')} now.\n`)
+  console.log(`When your modifications are complete, you can launch the hook by executing the following command:\n`)
+  console.log(`${chalk.cyan('node listener.js')}\n`)
+  console.log(`You can also use other tools to deploy the service, for example ${chalk.underline('pm2')}, ${chalk.underline('nodemon')} or ${chalk.underline('forever')}.\n`)
+  console.log(`Thank you for using it and start coding happily! 😊`)
 }
 
 async function main () {
   program
     .version(packageJson.version)
 
-  await initial()
+  initial()
 }
 
 main()
 
 program.parse(process.argv)
+
